4e456a4028a4e1ab479bb3dc1c27296dc745d705
Nick Hulea <naeluh@gmail.com>
2018-08-08 19:53:14 -0700

    working site still needs contact and server but close

--- src/app/Home.js
+++ src/app/Home.js
@@ -2,21 +2,21 @@ import React from 'react'
 import { Link } from "react-router-dom";
 
 class Home extends React.Component {
-	render() {
-		return (
-    <div className="background-content" >
-    <section className="offset60">
-      <h1 id="title">Nick Hulea</h1>
-    </section>
-    <section className="offset60">
-      <p id="description" >Hello you have arrived at the website of Nick Hulea !</p>
-      <p>Samples of my work can be found <Link to="work" title="work" alt="work">here .</Link></p>
-      <p>If you would like to contact me or if you have any questions click <Link to="contact" title="contact" alt="contact" >here .</Link></p>
-    </section>
-    <div id="wrap"><div id="block"></div></div>
-  </div>
-  )
-}
+  render() {
+    return (
+      <div className="background-content" >
+        <section className="offset60">
+          <h1 id="title">Nick Hulea</h1>
+        </section>
+        <section className="offset60">
+          <p id="description" >Hello you have arrived at the website of Nick Hulea !</p>
+          <p>Samples of my work can be found <Link to="work" title="work" alt="work">here .</Link></p>
+          <p>If you would like to contact me or if you have any questions click <Link to="contact" title="contact" alt="contact" >here .</Link></p>
+        </section>
+        <div id="wrap"><div id="block"></div></div>
+      </div>
+    )
+  }
 }
 
 export default Home
